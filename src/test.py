6ad866544ff89f8cc14cfcf56359f57eefa67a96
iPonzu <joao_ignaciocastro1603@hotmail.com>
2022-09-02 00:53:08 -0300

    first commit

--- src/test.py
+++ src/test.py
@@ -1,15 +1,5 @@
-numeros = [1,3,6]
-print(sum(numeros))
-#len()
-paises = ['Brasil','Argentina','Portugal','Itália']
-print(len(paises))
-#type()
-professores = ['Carla','Eduardo','Felipe','Lucas']
-estacoes = ('Primavera','Outono','Verão')
-cliente = {
-    'Nome': 'Fabio Garcia',
-    'email':'fabiogarcia@gmail.com'
-}
-print(type(professores)) #list
-print(type(estacoes)) #tuple
-print(type(cliente)) #dict
+nomes = ["Lucas, Fernando, Felipe, José, Rafael, Gabriel"]
+nomes.insert(1, "Marcos")
+print(nomes) #nomes
+print(len(nomes)) #tamanho da lista
+print(type(nomes))# o que ele é
